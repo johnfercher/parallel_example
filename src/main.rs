72a95bda1be216e94b13311bc8aa9b525de374d9
Johnathan Fercher <johnathanfercher22@gmail.com>
2019-04-08 16:58:30 -0300

    exemplo normal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate rand;
+
 fn main() {
-    println!("Hello, world!");
+    let colecao_aleatoria: [u16; 32] = rand::random();
+
+    let colecao_de_pares = colecao_aleatoria
+        .to_vec()
+        .into_iter()
+        .filter(|x| x % 2 == 0)
+        .collect::<Vec<u16>>();
+
+    println!("{:?}", colecao_aleatoria);
+    println!("{:?}", colecao_de_pares);
 }
