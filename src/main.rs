4bc050777d8c77e8981aae7c159d80398699de11
Johnathan Fercher <johnathanfercher22@gmail.com>
2019-04-08 17:20:52 -0300

    paralelo 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate rand;
+extern crate rayon;
+
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 fn main() {
     let colecao_aleatoria: [u16; 32] = rand::random();
 
     let colecao_de_pares = colecao_aleatoria
         .to_vec()
-        .into_iter()
+        .into_par_iter()
         .filter(|x| x % 2 == 0)
         .collect::<Vec<u16>>();
 
