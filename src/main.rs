31238c6b03e6e112f81ac9e7247db7716167a931
Johnathan Fercher <johnathanfercher22@gmail.com>
2019-04-08 17:34:04 -0300

    paralelo 2

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,25 @@ extern crate rand;
 extern crate rayon;
 
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use rayon::ThreadPoolBuilder;
 
 fn main() {
     let colecao_aleatoria: [u16; 32] = rand::random();
 
-    let colecao_de_pares = colecao_aleatoria
-        .to_vec()
-        .into_par_iter()
-        .filter(|x| x % 2 == 0)
-        .collect::<Vec<u16>>();
+    let thread_pool = ThreadPoolBuilder::new()
+        .num_threads(2)
+        .build()
+        .unwrap();
 
     println!("{:?}", colecao_aleatoria);
+
+    let colecao_de_pares = thread_pool.install(|| {
+        colecao_aleatoria
+            .to_vec()
+            .into_par_iter()
+            .filter(|x| x % 2 == 0)
+            .collect::<Vec<u16>>()
+    });
+
     println!("{:?}", colecao_de_pares);
 }
